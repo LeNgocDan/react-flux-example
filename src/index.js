6ba8488c1c7d5b3220764380ea40006d7e0b7286
LeNgocDan <Linuss1908@gmail.com>
2020-12-27 19:52:57 +0700

    create HomePage component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import ReactDOM from "react-dom";
+import HomePage from "./components/HomePage";
+
+ReactDOM.render(<HomePage />, document.getElementById("root"));
