5915fb19e942bf56191c04fb4acdf7780d726e06
LeNgocDan <Linuss1908@gmail.com>
2020-12-28 21:09:57 +0700

    config react-router

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,15 @@ import About from "./About";
 import Header from "./common/Header";
 import HomePage from "./HomePage";
 import CoursesPage from "./CoursesPage";
+import { Route } from "react-router-dom";
 
 function App() {
-  function getPage() {
-    const path = window.location.pathname;
-    if (path === "/courses") return <CoursesPage />;
-    if (path === "/about") return <About />;
-    return <HomePage />;
-  }
-
   return (
     <div>
       <Header />
-      {getPage()}
+      <Route path="/" component={HomePage} />
+      <Route path="/about" component={About} />
+      <Route path="/courses" component={CoursesPage} />
     </div>
   );
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,6 +5,9 @@ function HomePage() {
     <div className="jumbotron">
       <h1>Administration</h1>
       <p>React, Flux and React Router for web app.</p>
+      <a href="/about" className="btn btn-primary">
+        About
+      </a>
     </div>
   );
 }
